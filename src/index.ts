766ab5a4ba5aa0c22325e06c712f67eaf312241d
alex-mxrz <110593211+alex-mxrz@users.noreply.github.com>
2024-05-31 19:18:34 +0300

    Create telegram-client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+// import { TelegramClient } from './telegram-client';
+
 (async () => {
-  //
+  // TelegramClient.configureTdLib();
+
+  // const client = new TelegramClient(
+  //   {
+  //     apiHash: 'dfd4909a4e3e89f6d285185711ab77d9',
+  //     apiId: 29554815
+  //   },
+  //   {
+  //     botName: 'Blum',
+  //     webappUrl: ''
+  //   }
+  // );
+
+  // const clientSession = await client.initialize();
 })();
---
+++ src/telegram-client/types.d.ts
@@ -0,0 +1,23 @@
+export interface BotInfo {
+  readonly botId: number;
+  readonly chatId: number;
+}
+
+export interface ClientConfig {
+  readonly databasePath?: string;
+  readonly filesPath?: string;
+
+  readonly apiHash: string;
+  readonly apiId: number;
+}
+
+export interface ClientInfo {
+  readonly botName: string;
+  readonly webappUrl: string;
+}
+
+export interface ClientSession {
+  readonly botAccountId: number;
+  readonly botChatId: number;
+  readonly username: string;
+}
